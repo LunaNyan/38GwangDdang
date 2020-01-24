d93fe8a536842f22dc4c73e8b62fab26ad2f24f3
lunanyan <chkong1998@gmail.com>
2020-01-25 04:46:59 +0900

    fix judge, add deck select, card interpretation

--- modules/m_calc.py
+++ modules/m_calc.py
@@ -27,34 +27,44 @@ def make_deck():
     random.shuffle(deck)
     return deck
 
+def select_deck(deck):
+    # 주어진 패 목록에서 랜덤한 패 2장을 뽑고 리스트에서 삭제합니다.
+    decka_ind = random.randint(0, len(deck)-1)
+    decka_val = deck[decka_ind]
+    del deck[decka_ind]
+    deckb_ind = random.randint(0, len(deck)-1)
+    deckb_val = deck[deckb_ind]
+    del deck[deckb_ind]
+    return [decka_val, deckb_val]
+
 def judge(a, b):
     # [월수, 끗수]로 된 두 패를 받아 조합 결과를 산출합니다.
     # 끗수 설명
     # - 0 : 기타
     # - 1 : 광
     # - 2 : 열끗
-    if (a[0] == 3 and b[0] == 8) or (a[0] == 8 and b[0] == 3) and a[1] == 1 and b[1] == 1: #광땡
+    if ((a[0] == 3 and b[0] == 8) or (a[0] == 8 and b[0] == 3)) and a[1] == 1 and b[1] == 1: #광땡
         sd_power_text = "3 - 8 광땡!"
-        return 28
-    elif (a[0] == 1 and b[0] == 8) or (a[0] == 8 and b[0] == 1) and a[1] == 1 and b[1] == 1:
+        sd_power = 28
+    elif ((a[0] == 1 and b[0] == 8) or (a[0] == 8 and b[0] == 1)) and a[1] == 1 and b[1] == 1:
         sd_power_text = "1 - 8 광땡!"
-        return 27
-    elif (a[0] == 1 and b[0] == 3) or (a[0] == 3 and b[0] == 1) and a[1] == 1 and b[1] == 1:
+        sd_power = 27
+    elif ((a[0] == 1 and b[0] == 3) or (a[0] == 3 and b[0] == 1)) and a[1] == 1 and b[1] == 1:
         sd_power_text = "1 - 3 광땡!"
-        return 26
+        sd_power = 26
     elif (a[0] == 9 and b[0] == 4) or (a[0] == 4 and b[0] == 9): #특수족보
         if a[1] == 2 and b[1] == 2:
             sd_power_text = "멍텅구리구사"
-            return -1
+            sd_power = -1
         else:
             sd_power_text = "구사"
-            return -2
+            sd_power = -2
     elif (a[0] == 7 and b[0] == 4) or (a[0] == 4 and b[0] == 7) and a[1] == 2 and b[1] == 2:
         sd_power_text = "7 - 4 암행어사"
-        return -3
+        sd_power = -3
     elif (a[0] == 7 and b[0] == 3 and a[1] == 2 and b[1] == 1) or (a[0] == 3 and b[0] == 7 and a[1] == 1 and b[1] == 2):
         sd_power_text = "7 - 3 땡잡이"
-        return -4
+        sd_power = -4
     elif a[0] == b[0]: #땡
         if a[0] == 10:
             sd_power_text = "장땡"
@@ -76,7 +86,7 @@ def judge(a, b):
             sd_power_text = "이땡"
         elif a[0] == 1:
             sd_power_text = "일땡"
-        sd_power = 15 + a
+        sd_power = 15 + a[0]
     elif (a[0] == 1 and b[0] == 2) or (a[0] == 2 and b[0] == 1): #삥
         sd_power_text = "알리"
         sd_power = 15
@@ -100,10 +110,7 @@ def judge(a, b):
         xcut = int(xcuta)
         # 주의사항
         # - 두 패의 월 조합이 10인 경우, 끗수가 0이므로 망통으로 처리됩니다.
-        #   따라서 끗 수가 10인 경우는 있을 수 없으며, 해당 상황 발생 시 ValueError로 셀프 크래시를 발동합니다.
-        if xcut == 10:
-            raise ValueError
-        elif xcut == 9:
+        if xcut == 9:
             sd_power_text = "갑오"
         elif xcut == 8:
             sd_power_text = "여덟끗"
@@ -125,3 +132,13 @@ def judge(a, b):
             sd_power_text = "망통"
         sd_power = xcut
     return [sd_power, sd_power_text]
+
+def interpret_card(card):
+    if card[1] == 0:
+        card_type = ""
+    elif card[1] == 1:
+        card_type = " 광"
+    elif card[1] == 2:
+        card_type = " 열끗"
+    return str(card[0]) + "월" + card_type
+
